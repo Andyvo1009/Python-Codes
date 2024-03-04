10c4dce658e310686a0a08a80b35dd42aed3d3a5
Andyvo1009 <141822131+Andyvo1009@users.noreply.github.com>
2024-03-05 02:39:46 +0700

    Update Hangman.py
    Add more arts and vocabularies

--- Hangman.py
+++ Hangman.py
@@ -1,7 +1,10 @@
 import random
-words = "ape cat dog baboon elephant giraffe apple coconut monkey rubik mice mouse pineapple android apple house fence python grail zerg protoss terran llama fire policeman mamerto smad zebra lion luffy nigga universidad".split()
-word=words[random.randint(0,len(words)-1)]
+import vocabulary
+import art
+word=random.choice(vocabulary.words)
 word_count= len(word)
+print(art.logo)
+arts=art.stages
 n=10
 lives=5
 print("GAME START!")
@@ -9,12 +12,12 @@ print("--------------------------------------")
 blanks=[]
 for i in range(word_count):
   blanks.append("_")
-for i in range(word_count):
-  print(blanks[i],end= " ")
+print(blanks)
+print("\n"+"\n")
 win=True
 while(win==True):
   n-=1
-  guess= input(str("\nEnter a letter: "))
+  guess= input(str("Enter a letter: "))
   print("--------------------------------------")
   if guess in word:
     temp=0
@@ -23,11 +26,12 @@ while(win==True):
         temp+=1
         blanks[i]=guess
     print(f"There are {temp} {guess} in this word")
-    for i in range(word_count):
-      print(blanks[i],end= " ")
+    
+    print(blanks)
     print(f"\n{lives} lives left")
   else:
     lives-=1
+    print(arts[lives])
     print("Your guess is wrong")
     print(f"{lives} lives left")
   print("----------------------------------------------")
@@ -36,9 +40,9 @@ while(win==True):
   if '_' not in blanks:
     break
   
-  
+print(f"The answer is {word}")
 if(win==False):
-  print("LOST")
+  print("YOU LOST")
 else:
-  print("WIN")
+  print("YOU WON")
 print("GAME OVERRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR")
